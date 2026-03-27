a1b5e9234aa42b6b3806dd6239f458b5bfbde40b
yoonguri988 <alice951816@gmail.com>
2026-03-28 02:10:38 +0900

    refactor: Define user model

--- track003_java/bankproject/src/com/the703/v1/User.java
+++ track003_java/bankproject/src/com/the703/v1/User.java
@@ -1,39 +1,48 @@
 package com.the703.v1;
 
 public class User {
-	  String id;
-	  String pw;
-	  int age;
-	  long account;
-	  
-	  public User(String id, String pw, int age, long account) {
-		  this.id = id;
-		  this.pw = pw;
-		  this.age = age;
-		  this.account = account;
-	  }
-	  
-	  public boolean isExists(String id, String pw) {
-		  boolean result = false;
-		  result = this.id.equals(id) && this.pw.equals(pw);
-		  return result;
-	  }
-	  
-	  public boolean isEmpty(long account) {
-		  return this.account >= account;
-	  }
-	  
-	  public void selectOneById(String id) {
-		  System.out.println("=== 계좌 조회");
-		  System.out.println("ID: "+this.id);
-		  System.out.println("PASS: "+this.pw);
-		  System.out.println("나이: "+this.age);
-		  System.out.println("잔액: "+this.account);
-	  }
-	  
-	  public long updateAccountById(String id, long account) {
-		  this.account += account;
-		  return this.account;
-	  }
+	String userId;
+	String password;
+	int age;
+	long account;
 
+	public User(String userId, String password, int age, long account) {
+		this.userId = userId;
+		this.password = password;
+		this.age = age;
+		this.account = account;
 	}
+	
+	// getter, setter
+	public String getUserId() {
+		return this.userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public long getAccount() {
+		return this.account;
+	}
+	
+	public void setAccount(long account) {
+		this.account = account;
+	}
+}
