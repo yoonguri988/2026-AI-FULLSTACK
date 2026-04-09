df275668991ba236a648538c0fac59cf6b186b7e
yoonguri988 <alice951816@gmail.com>
2026-04-10 00:25:22 +0900

    feat: 삼중 for -> 이중 for

--- track003_java/java2026/src/com/the703/basic007_ex/For2Ex014.java
+++ track003_java/java2026/src/com/the703/basic007_ex/For2Ex014.java
@@ -2,6 +2,29 @@ package com.the703.basic007_ex;
 
 public class For2Ex014 {
 	public static void main(String[] args) {
+//	for (int j = 1; j < 10; j++) {
+//		for (int i = 2; i < 5; i++) {
+//			System.out.print(i);
+//		}
+//		System.out.println();
+//	}
+//
+//	System.out.println();
+//	for (int j = 1; j < 10; j++) {
+//		for (int i = 5; i < 8; i++) {
+//			System.out.print(i);
+//		}
+//		System.out.println();
+//	}
+//	System.out.println();
+//	for (int j = 1; j < 10; j++) {
+//		for (int i = 8; i < 10; i++) {
+//			System.out.print(i);
+//		}
+//		System.out.println();
+//	}
+//	System.out.println();
+		
 		System.out.println("ver-1:: for");
 		
 		for(int k = 2; k < 10; k+=3) {
@@ -14,29 +37,19 @@ public class For2Ex014 {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("ver-2:: for");
 
-//		for (int j = 1; j < 10; j++) {
-//			for (int i = 2; i < 5; i++) {
-//				System.out.print(i);
-//			}
-//			System.out.println();
-//		}
-//
-//		System.out.println();
-//		for (int j = 1; j < 10; j++) {
-//			for (int i = 5; i < 8; i++) {
-//				System.out.print(i);
-//			}
-//			System.out.println();
-//		}
-//		System.out.println();
-//		for (int j = 1; j < 10; j++) {
-//			for (int i = 8; i < 10; i++) {
-//				System.out.print(i);
-//			}
-//			System.out.println();
-//		}
-//		System.out.println();
+		for(int i = 1; i <= 27; i++) { // (3*9)*3
+			int row = ((i-1)/9)*3+2; // 3n+2
+			int col = (i-1)%9+1;
+			for (int j = row; j < row+3; j++) {
+				if(j == 10) break;
+				System.out.printf("%d*%d=%d\t",col,j,col*j);
+			}
+			System.out.println();
+			if(col == 9) System.out.println();
+		}
 	}
 }
 /*
